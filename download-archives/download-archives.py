58163597d845f5e68f4e40a6cdd41a5b088d391d
NicholasPiano <nicholas.d.piano@gmail.com>
2014-12-02 00:45:15 +0000

    Download archives works.

--- download-archives/download-archives.py
+++ download-archives/download-archives.py
@@ -4,7 +4,9 @@ import re
 import sys
 import mechanize as mz
 from optparse import OptionParser
-from HTMLParser import HTMLParser
+import pprint
+import shutil as sh
+import os
 
 '''
 https://docs.python.org/2/library/htmlparser.html
@@ -14,40 +16,40 @@ http://wwwsearch.sourceforge.net/mechanize/
 
 #script to download archives from behind a password.
 
+# <tr class="search_bg1 tdpanel" id="rows6">
+#   <td align="center" valign="middle">
+#     <a href="http://www.postmyfiles.com/voxgen/home.php?foId=2080"  class="link">
+#       <img src="http://www.postmyfiles.com/images/folder.gif" alt="Folder" title="Folder" border="0"  />
+#     </a>
+#   </td>
+#   <td align="center" valign="top">
+#     <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
+#   </td>
+#   <td align="left" valign="middle" class="tabeltext2">
+#     <a href="http://www.postmyfiles.com/voxgen/home.php?foId=2080"  class="text_product">On Hold 2013 - PCM Mono_3min Seg</a>
+#   </td>
+#   <td align="center" valign="top">
+#     <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
+#   </td>
+#   <td width="50" align="left" valign="middle" class="tabeltext">15-02-2013</td>
+#   <td width="1" align="center" valign="top"><img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" /></td>
+#   <td align="left" valign="middle" class="tabeltext2">0 Dir(s),<br />1 File(s).</td>
+#   <td width="1" align="center" valign="top">
+#     <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
+#   </td>
+#   <td align="left" valign="middle" class="tabeltext2">37.2 MB</td>
+#   <td width="1" align="center" valign="top">
+#     <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
+#   </td>
+#   <td align="left" valign="middle" class="tabeltext2">
+#     <a href="http://www.postmyfiles.com/voxgen/home.php?action=sharedoc&foId=2080" class="text_product">
+#       <strong>Share</strong>
+#     </a>
+#   </td>
+#   <td width="1" align="center" valign="top">
+#     <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
+#   </td>
 
-<tr class="search_bg1 tdpanel" id="rows6">
-  <td align="center" valign="middle">
-    <a href="http://www.postmyfiles.com/voxgen/home.php?foId=2080"  class="link">
-      <img src="http://www.postmyfiles.com/images/folder.gif" alt="Folder" title="Folder" border="0"  />
-    </a>
-  </td>
-  <td align="center" valign="top">
-    <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
-  </td>
-  <td align="left" valign="middle" class="tabeltext2">
-    <a href="http://www.postmyfiles.com/voxgen/home.php?foId=2080"  class="text_product">On Hold 2013 - PCM Mono_3min Seg</a>
-  </td>
-  <td align="center" valign="top">
-    <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
-  </td>
-  <td width="50" align="left" valign="middle" class="tabeltext">15-02-2013</td>
-  <td width="1" align="center" valign="top"><img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" /></td>
-  <td align="left" valign="middle" class="tabeltext2">0 Dir(s),<br />1 File(s).</td>
-  <td width="1" align="center" valign="top">
-    <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
-  </td>
-  <td align="left" valign="middle" class="tabeltext2">37.2 MB</td>
-  <td width="1" align="center" valign="top">
-    <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
-  </td>
-  <td align="left" valign="middle" class="tabeltext2">
-    <a href="http://www.postmyfiles.com/voxgen/home.php?action=sharedoc&foId=2080" class="text_product">
-      <strong>Share</strong>
-    </a>
-  </td>
-  <td width="1" align="center" valign="top">
-    <img src="http://www.postmyfiles.com/images/line_icon2.gif" width="1" height="35" />
-  </td>
 
 def main(options):
   #get browser
@@ -60,18 +62,48 @@ def main(options):
   b["userName"] = "sdg"
   b["userPass"] = options.password
   b.submit()
-  base_folder_url = 'http://www.postmyfiles.com/voxgen/home.php?foId=2609'
-  base_response = b.open(base_folder_url)
 
-  ht = Ht()
-  ht.feed(base_response.read())
+  #tuning folder
+  b.follow_link(text_regex=options.root, nr=0)
 
-  #past login, navigate to folder
+  #table
+  rx = re.compile(r'http://www\.postmyfiles\.com/download_file\.php\?fileid=(?P<id>[0-9]+)')
+  rev = 'http://www.postmyfiles.com/download_file.php?fileid=%d'
 
+  links = []
+  for link in b.links(url_regex='foId'):
+    attr_dict = {a[0]:a[1] for a in link.attrs}
+    if 'class' in attr_dict and attr_dict['class'] == 'text_product' and 'share' not in link.url:
+      if link.url not in [l.url for l in links]:
+        links.append(link)
+
+  archive_links = {}
+  for link in links:
+    b.open(link.url)
+    if link.text not in archive_links:
+      archive_links[link.text] = []
+
+      for sub_link in b.links(url_regex='fileid'):
+        attr_dict = {a[0]:a[1] for a in sub_link.attrs}
+        if 'class' in attr_dict and attr_dict['class'] == 'text_product' and 'share' not in sub_link.url:
+          if sub_link.url not in [l.url for l in archive_links[link.text]]:
+            archive_links[link.text].append(sub_link)
+
+  base_path = os.path.join('/','Users','nicholaspiano','code','scripts','download-archives','data')
+  for link in archive_links:
+    #create folder for download
+    if not os.path.isdir(os.path.join(base_path, link)):
+      os.mkdir(os.path.join(base_path, link))
+
+    #loop through files
+    for archive in archive_links[link]:
+      if not os.path.exists(os.path.join(base_path, link, archive.text)):
+        b.retrieve(archive.url, os.path.join(base_path, link, archive.text))
 
 
 if __name__ == "__main__":
   parser = OptionParser()
   parser.add_option("-p", "--password", dest="password")
+  parser.add_option("-r", "--root", dest="root")
   (options, args) = parser.parse_args()
   main(options)

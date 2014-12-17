5b6eadf36979bd6f7ffdc3fc442ada24da168203
NicholasPiano <nicholas.d.piano@gmail.com>
2014-12-17 01:10:51 +0000

    Need to test translate

--- eckoh/pattern.py
+++ eckoh/pattern.py
@@ -8,25 +8,19 @@ import random
 letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 letter = re.compile(r'^[a-zA-Z]$')
 
-minFormatFreq = 100
-maxPerFormat = 50
+exclusion_threshold = 100
+max_per_format = 50
 
 class Licence(object):
 
   def __init__(self, input_string):
     self.string = input_string
     self.pattern = ''
-    self.sequence = []
-    for group in input_string.split(' '):
-      for char in group:
-        self.sequence.append(char)
-        if re.search(letter, char) is not None:
-          self.pattern += 'L'
-        else:
-          self.pattern += 'N'
-      if len(input_string.split(' '))>1:
-        self.pattern += 'S'
-        self.sequence.append(' ')
+    for char in input_string:
+      if char.isalpha():
+        self.pattern += 'L'
+      else:
+        self.pattern += 'N'
 
 def strip(string):
   groups = string.split('\t')
@@ -38,95 +32,10 @@ def generate_with_pattern(pattern):
     string += random.choice(letters) if char=='L' else str(random.randint(0,9))
   return string
 
-def changeDigits(three_string):
-  print three_string + "\n"
-  
-  return ""
+ones = ['<ruleref uri=\"Digits.xml#Zero\"/>', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+tens = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
 
-def searchThreeDigitsOnly(input_string):
-  string = ''
-  strjoin = ' ';
-  build_string = strjoin.join(input_string.split(' '))
-  matchAlpha = re.search('(\w)', input_string)
-  if (matchAlpha):
-    matchThree1 = re.search('^((\w|\s)+)(\d \d \d)$', build_string)
-    if (matchThree1):
-      string = re.sub(r'^((\w|\s)+)(\d \d \d)$', changeDigits, build_string)
-    else:
-      matchThree2 = re.search('^(\d \d \d)((\w|\s)+)$', build_string)
-      if (matchThree2):
-        string = matchThree2(0)
-      else:
-        matchThree3 = re.search('((\w|\s)+)(\d \d \d)((\w|\s)+)', build_string)
-        if (matchThree3):
-           string = matchThree3(0)
-  else:
-    if (length(input_string) == 3):
-      string = matchThree3(0)
-      
-  return string
-
-def searchTwoDigitsOnly(input_string):
-  string = ''
-  for char in pattern:
-    string += random.choice(letters) if char=='L' else str(random.randint(0,9))
-  return string
-
-def getWord(input_string):
-    retval = ''
-
-    if input_string == '0':
-      retval = "<ruleref uri=\"Digits.xml#Zero\"/>"
-    
-    if input_string == '1':
-      retval = "one"
-    
-    if input_string == '2':
-      retval = "two"
-    
-    if input_string == '3':
-      retval = "three"
-    
-    if input_string == '4':
-      retval = "four"
-    
-    if input_string == '5':
-      retval = "five"
-    
-    if input_string == '6':
-      retval = "six"
-    
-    if input_string == '7':
-      retval = "seven"
-    
-    if input_string == '8':
-      retval = "eight"
-    
-    if input_string == '9':
-      retval = "nine"
-
-    return ["<ruleref uri=\"Digits.xml#Zero\"/>", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"][int(input_string)]
-
-def stringBuilder (build_string, elem, separator):
-
-  if (build_string == ""):
-	build_string = elem
-  else:
-	build_string = build_string + separator + elem
-
-  return build_string
-
-def getGramLine (input_string):
-
-  build_string = ''
-  for group in input_string.split(' '):
-    for char in group:
-      if char.isalpha():
-        build_string = stringBuilder(build_string, char, ' ')
-      else:
-        build_string = stringBuilder(build_string, getWord(char), ' ')
-
-  return build_string
+def translate():
 
 #input from data
 
@@ -157,9 +66,6 @@ print(unique_patterns)
 
 #for each unique pattern, generate a random string that conforms to the pattern, but is not currently in the data set
 #make 100 for each and save to output directory with the pattern as the filename.txt
-
-#threshold
-exclusion_threshold = minFormatFreq #patterns with a frequency less than this number are excluded
 
 gram_preamble = ''
 gram_preamble += "<\?xml version=\"1.0\" encoding=\"ISO-8859-1\" \?>\n"
@@ -213,7 +119,7 @@ gram_postamble += "\t</one-of>\n"
 gram_postamble += "</rule>\n\n"
 gram_postamble += "</grammar>\n"
 
-output_path = 'decoy'
+output_path = 'grammar'
 if not os.path.exists(output_path): #creates output directory if necessary
   os.makedirs(output_path)
 
@@ -224,7 +130,7 @@ for pattern in unique_patterns:
   frequency = len(data)
   if frequency >= exclusion_threshold:
     file_name = os.path.join(output_path, pattern+'_decoy.xml')
- #   print('pattern: %s above threshold (%d) with frequency %d, writing to %s'%(pattern, exclusion_threshold, frequency, file_name))
+   print('pattern: %s above threshold (%d) with frequency %d, writing to %s'%(pattern, exclusion_threshold, frequency, file_name))
     decoy_set = []
     for i in range(maxPerFormat):
       test_decoy = generate_with_pattern(pattern)
@@ -234,22 +140,11 @@ for pattern in unique_patterns:
 
     with open(file_name, 'w') as f:
       f.write(gram_preamble)
-      
+
       for decoy in decoy_set:
-        build_string = "\t\t<item> " + getGramLine(decoy.lower()) + " <tag>VALUE=\"" + decoy.lower() + "\";</tag></item>\n"
-        f.write(build_string)
-        threeNumLine = searchThreeDigitsOnly(decoy.lower())
-        if (threeNumLine != ""): # three
-          build_string = "\t\t<item> " + threeNumLine + " <tag>VALUE=\"" + decoy.lower() + "\";</tag></item>\n"
-          f.write(build_string)
-        else:
-          twoNumLine = searchTwoDigitsOnly(decoy.lower())
-          if (twoNumLine != ""): # two
-            build_string = "\t\t<item> " + twoNumLine + " <tag>VALUE=\"" + decoy.lower() + "\";</tag></item>\n"
-            f.write(build_string)
-            
-#        f.write(decoy + '\n')
+        line = r'\t\t<item>%s<tag>VALUE="%s";</tag></item>\n' %(translate(decoy), decoy.lower())
 
       f.write(gram_postamble)
   else:
     print('pattern: %s below threshold (%d) with frequency %d, EXCLUDING'%(pattern, exclusion_threshold, frequency))
+
